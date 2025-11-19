3e4e8692718d500c09fd9a88def96a9f04e704d7
u-sylvie <sylvieuwera369@gmail.com>
2025-11-19 14:43:20 +0200

    Build the Contacts component

---
+++ src/components/Contact.jsx
@@ -0,0 +1,97 @@
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+import { SplitText } from "gsap/all";
+import React from "react";
+
+import FooterRight from "../../public/images/footer-right-leaf.png";
+import FooterLeft from "../../public/images/footer-left-leaf.png";
+import { openingHours, socials } from "../../constants";
+
+function Contact() {
+  useGSAP(() => {
+    const titleSplit = SplitText.create("#contact h2", { type: "words" });
+
+    const timeline = gsap.timeline({
+      scrollTrigger: {
+        trigger: "#contact",
+        start: "top center",
+      },
+      ease: "power1.inOut",
+    });
+    timeline
+      .from(titleSplit.words, {
+        opacity: 0,
+        yPercent: 100,
+        stagger: 0.02,
+      })
+      .from("#contact h3, #contact p", {
+        opacity: 0,
+        yPercent: 100,
+        stagger: 0.02,
+      })
+      .to("#f-right-leaf", {
+        y: "-50",
+        duration: 1,
+        ease: "power1.inOut",
+      })
+      .to(
+        "#f-left-leaf",
+        {
+          y: "-50",
+          duration: 1,
+          ease: "power1.inOut",
+        },
+        "<"
+      );
+  });
+  return (
+    <footer id="contact">
+      <img src={FooterRight} alt="" id="f-right-leaf" />
+      <img src={FooterLeft} alt="" id="f-left-leaf" />
+
+      <div className="content">
+        <h2>Where to Find Us</h2>
+
+        <div>
+          <h3>Visit Our Bar</h3>
+          <p>456, Raq Blvd. #404, Kigali, KN A20</p>
+        </div>
+
+        <div>
+          <h3>Contact Us</h3>
+          <p>+25078032839</p>
+          <p>mbonabihita@gmail.com</p>
+        </div>
+
+        <div>
+          <h3>Open Every Day</h3>
+          {openingHours.map((time) => (
+            <p key={time.day}>
+              {time.day} : {time.time}
+            </p>
+          ))}
+        </div>
+
+        <div>
+          <h3>Socials</h3>
+
+          <div className="flex-center gap-5">
+            {socials.map((social) => (
+              <a
+                key={social.name}
+                href={social.url}
+                target="_blank"
+                rel="noopener noreferrer"
+                aria-label={social.name}
+              >
+                <img src={social.icon} alt="" />
+              </a>
+            ))}
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+}
+
+export default Contact;
