f2c333dc6324a28770ff3504c8b6497bff3b2138
u-sylvie <sylvieuwera369@gmail.com>
2025-11-19 14:43:52 +0200

    Update the app settings

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,9 @@ import { ScrollTrigger, SplitText } from 'gsap/all'
 import Hero from './components/Hero'
 import Cocktails from './components/Cocktails'
 import About from './components/About'
+import Art from './components/Art'
+import Menu from './components/Menu'
+import Contact from './components/Contact'
 
 gsap.registerPlugin(ScrollTrigger, SplitText)
 function App() {
@@ -16,6 +19,9 @@ function App() {
       <Hero />
       <Cocktails />
       <About />
+      <Art />
+      <Menu />
+      <Contact />
     </main>
   )
 }
